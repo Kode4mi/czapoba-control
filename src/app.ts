4435cf9897acacc4d53c0e909be3831563ecba30
KotKamil <62114754+KotKamil@users.noreply.github.com>
2023-09-21 20:39:45 +0200

    Sign in and do a simple health check

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,38 @@
 // src/app.ts
 import express from 'express';
+import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = 5249;
+let cookies: string[] = [""];
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/', async (req, res) => {
+    let status: number = 0;
+    try {
+        await axios.get('http://localhost/api/products/health',
+            {
+                headers: {
+                    Cookie: cookies
+                }
+            }).then(res => status = res.status)
+    } catch (error: any){
+        status = error.response.status;
+        console.log(error.response.status)
+    }
+    res.send(JSON.stringify(status));
 });
 
-app.listen(port, () => {
+app.listen(port,async () => {
+    try{
+        await axios.post('http://localhost/api/users/signin', {"email": "admin@czapoba.pl", "password": "admin"})
+            .then((response) => {
+                const setCookie = response.headers["set-cookie"];
+                if (setCookie === undefined) return;
+                cookies = setCookie;
+            })
+        console.log("Successfully logged in to czapoba")
+    } catch (e){
+        console.log(e)
+    }
     console.log(`Server is running on port ${port}`);
 });
